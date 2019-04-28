4e1431e2c2585dceaebd65b2962ee5d369be634d
mnemonic <41833021+mnemonic-re@users.noreply.github.com>
2019-04-28 17:08:31 +0200

    Update guessme.cpp

--- memory_intro/guessme_game/guessme.cpp
+++ memory_intro/guessme_game/guessme.cpp
@@ -4,16 +4,6 @@
 #include <iostream>
 #include <time.h>
 
-/*
-Compile Options to reduce the size of binary and remove security measures for practice
-
-- C\C++ -> Optimization -> Optimization = DISABLED
-- C\C++ -> Code Generation -> Security Check = Disable Security Check (/GS-)
-- Linker -> Advanced
-Randomized Base Address = No (/DYNAMICBASE:NO)
-Data Execution Prevention (DEP) = No (/NXCOMPAT:NO)
-*/
-
 int main()
 {
 	__asm nop;
